ae2aa0d3ad682546dbd4461e5f5134b474bca1a9
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-07-31 20:59:57 -0300

    Model

--- src/model/Pokemon.ts
+++ src/model/Pokemon.ts
@@ -2,4 +2,76 @@ export interface Pokemon {
   id?: number,
   name: string;
   url?: string;
+  abilities: Ability[]
+  base_experience: number
+  forms: Form[]
+  game_indices: Index[]
+  height: number
+  held_items: any[]
+  is_default: boolean
+  location_area_encounters: string
+  order: number
+  past_types: any[]
+  species: Species
+  sprites: Sprites
+  weight: number
+}
+
+export interface Ability {
+  ability: Ability2
+  is_hidden: boolean
+  slot: number
+}
+
+export interface Ability2 {
+  name: string
+  url: string
+}
+
+export interface Form {
+  name: string
+  url: string
+}
+
+export interface Index {
+  game_index: number
+  version: Version
+}
+
+export interface Version {
+  name: string
+  url: string
+}
+
+
+export interface VersionGroupDetail {
+  level_learned_at: number
+  move_learn_method: MoveLearnMethod
+  version_group: VersionGroup
+}
+
+export interface MoveLearnMethod {
+  name: string
+  url: string
+}
+
+export interface VersionGroup {
+  name: string
+  url: string
+}
+
+export interface Species {
+  name: string
+  url: string
+}
+
+export interface Sprites {
+  back_default: string
+  back_female: any
+  back_shiny: string
+  back_shiny_female: any
+  front_default: string
+  front_female: any
+  front_shiny: string
+  front_shiny_female: any
 }

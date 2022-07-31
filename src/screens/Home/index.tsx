db21b4fa6d2d612a19dbc69b1c6c45d14334f4a7
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-07-31 19:31:20 -0300

    Fixing types

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { FlatList, ScrollView, View } from "react-native";
 import { Head, CustomCard } from '../../components'
+import { Pokemon } from "../../model/Pokemon";
 
 const fake_data = [
     {
@@ -11,18 +12,16 @@ const fake_data = [
 ]
 
 export const Home = () => {
-    const renderItem = ({ item }) => (
-        <CustomCard item={item} />
-    );
+
     return(
         <View style={{flex:1}}>
             <Head/>
             <ScrollView horizontal={false} style={{ padding: 20 }}>
              <FlatList
-                    data={fake_data}
-                    renderItem={renderItem}
-                    keyExtractor={item => item.id}
-                />
+                data={fake_data}
+                renderItem={({item}) => <CustomCard item={item} />}
+                keyExtractor={(pokemon: Pokemon, i) => pokemon.name}
+            />
 
             </ScrollView>
         </View>
